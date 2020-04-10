68b7cdb5b6fc3723c33f3f34390187878756740b
Chen Xuanyi <xuanyi.chen@sg.yokogawa.com>
2020-04-10 21:34:36 +0800

    edit validateservice hook

--- codedeploy/4-validate-service.sh
+++ codedeploy/4-validate-service.sh
@@ -4,7 +4,7 @@
 ipaddr=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 listencount=$(netstat -an | grep 4000 | grep LISTEN | wc -l)
 if [ "$listencount" -lt 1 ]; then
-    exit 1
-else
     exit 0
+else
+    exit 1
 fi
