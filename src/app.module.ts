3b8979222381d604fd58b9bd2b84835b80429940
Chen Xuanyi <xuanyi.chen@sg.yokogawa.com>
2020-04-13 14:03:57 +0800

    fixing jest test

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,18 @@ import { SystemModule } from './system/system.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "hmu-dev-instance.cima23xzs8ac.ap-southeast-1.rds.amazonaws.com",
+      port: 5432,
+      username: "hmu_dev_user",
+      password: "Password#123",
+      database: "hmu_dev_db",
+      entities: [
+        __dirname.includes("dist") ? "./**/*.entity.js" : "./**/*.entity.ts"
+      ],
+      synchronize: false
+    }),
     CommonModule,
     SystemModule,
     ConfigModule
