15539f62dc069faa9412197fd220d303a293ebba
Chen Xuanyi <xuanyi.chen@sg.yokogawa.com>
2020-05-10 09:45:20 +0800

    Increase more hashing lines to increase load.

--- src/system/user/user.service.ts
+++ src/system/user/user.service.ts
@@ -17,7 +17,18 @@ export class UserService {
         const users = await this.userRepository.getUsers(item);
         const currentDateTime = new Date();
         const stringToHash = JSON.stringify(users) + currentDateTime.getSeconds() + (Math.random() * 10) + (Math.random() * 10);
-        const hashResult = crypto.createHash('sha512').update(JSON.stringify(users) + currentDateTime.getSeconds()).digest("hex");
+        let hashResult = crypto.createHash('sha512').update(JSON.stringify(users) + currentDateTime.getSeconds()).digest("hex");
+        hashResult = crypto.createHash('sha512').update(hashResult + (Math.random() * 10)).digest("hex");
+        hashResult = crypto.createHash('sha512').update(hashResult + (Math.random() * 10)).digest("hex");
+        hashResult = crypto.createHash('sha512').update(hashResult + (Math.random() * 10)).digest("hex");
+        hashResult = crypto.createHash('sha512').update(hashResult + (Math.random() * 10)).digest("hex");
+        hashResult = crypto.createHash('sha512').update(hashResult + (Math.random() * 10)).digest("hex");
+        hashResult = crypto.createHash('sha512').update(hashResult + (Math.random() * 10)).digest("hex");
+        hashResult = crypto.createHash('sha512').update(hashResult + (Math.random() * 10)).digest("hex");
+        hashResult = crypto.createHash('sha512').update(hashResult + (Math.random() * 10)).digest("hex");
+        hashResult = crypto.createHash('sha512').update(hashResult + (Math.random() * 10)).digest("hex");
+        hashResult = crypto.createHash('sha512').update(hashResult + (Math.random() * 10)).digest("hex");
+        hashResult = crypto.createHash('sha512').update(hashResult + (Math.random() * 10)).digest("hex");
 
         return { hash: hashResult };
     }
