9c6348db93c73c83cde434f474c55ce39223c768
cxuanyi <cxuanyi@gmail.com>
2020-05-14 14:36:13 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
 
+// Testing Trigger CICD from guthub website 1
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors()
